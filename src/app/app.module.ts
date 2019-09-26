4217ca0eb9a4526bfa6548662a64807873d7093c
KzTTT <1440433105@qq.com>
2019-09-26 22:34:29 +0800

    feat: hero.service.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HerosComponent } from './heros/heros.component';
 import { HeroDetaisComponent } from './hero-detais/hero-detais.component';
+import { HeroService } from './hero.service';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,9 @@ import { HeroDetaisComponent } from './hero-detais/hero-detais.component';
     BrowserModule,
     FormsModule,
   ],
-  providers: [],
+  providers: [
+    HeroService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
